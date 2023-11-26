55330a99e9b61105e14adbde30f197bf0ce69b12
Evandro Pomatti <evandro.pomatti@gmail.com>
2023-11-26 13:00:23 -0300

    readme

--- README.md
+++ README.md
@@ -1,6 +1,23 @@
-# azure-monitor-alerts
-Azure Monitor Alerts
+# Azure Monitor Alerts
 
+Implementation for Azure Monitor Alerts
+
+<img src=".assets/monitor-alerts.png" />
+
+Create the infrastructure:
+
+```sh
+cp config/sample.tfvars .auto.tfvars
+```
+
+Create the infrastructure:
+
+```sh
+terraform init
+terraform apply -auto-approve
+```
+
+Upload blobs to trigger the alarm:
 
 ```sh
 az storage blob upload \
--- modules/eventhub/main.tf
+++ modules/eventhub/main.tf
@@ -2,7 +2,7 @@ resource "azurerm_eventhub_namespace" "default" {
   name                = "evhns-${var.workload}"
   location            = var.location
   resource_group_name = var.resource_group_name
-  sku                 = "Standard"
+  sku                 = "Basic"
   capacity            = 1
 }
 
@@ -12,18 +12,4 @@ resource "azurerm_eventhub" "default" {
   resource_group_name = var.resource_group_name
   partition_count     = 2
   message_retention   = 1
-
-  # capture_description {
-  #   enabled             = true
-  #   encoding            = "Avro"
-  #   interval_in_seconds = var.evh_interval_in_seconds
-  #   size_limit_in_bytes = var.evh_size_limit_in_bytes
-
-  #   destination {
-  #     name                = "EventHubArchive.AzureBlockBlob"
-  #     blob_container_name = azurerm_storage_container.events.name
-  #     storage_account_id  = azurerm_storage_account.default.id
-  #     archive_name_format = "{Namespace}/{EventHub}/{PartitionId}/{Year}/{Month}/{Day}/{Hour}/{Minute}/{Second}"
-  #   }
-  # }
 }
